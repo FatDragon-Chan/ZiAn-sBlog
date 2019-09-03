7cb6bdc9aeb5af06335263ea4ffd5a4006ff6887
陈子安 <fengfeng1995@vip.qq.com>
2019-09-03 18:37:55 +0800

    配合接口修改

--- api/index.js
+++ api/index.js
@@ -11,6 +11,8 @@ import { get, post } from './http'
 export default {
   // 分页查询文章
   selectArticle(params) {
+    if (!params.status) params.status = 0
+    console.log(params)
     return post(`http://localhost:3000/blog/selective`, params)
   },
   getAllClassify(params) {
